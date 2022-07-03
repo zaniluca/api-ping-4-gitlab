587db7f54177a7d99b6a1b40ee802c493e5d638c
Luca Zani <lucazani2002@gmail.com>
2022-07-03 22:57:09 +0200

    feat: added expo notifications

--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -1,13 +1,72 @@
 import { Router, Request } from "express";
-import type { WebhookPayload } from "../types";
+import type { Headers, PipelineStatus, WebhookPayload } from "../types";
 import hash from "object-hash";
 import { prisma } from "../../prisma/client";
-import type { User } from "@prisma/client";
+import type { Notification, User } from "@prisma/client";
 import multer from "multer";
 import { parseHeaders } from "../utils";
-import { NOTIFICATION_ESSENTIALS_FIELDS } from "../constants";
+import { Expo, ExpoPushMessage } from "expo-server-sdk";
 
 const router = Router();
+
+// optionally providing an access token if you have enabled push security
+const expo = new Expo({ accessToken: process.env.EXPO_ACCESS_TOKEN });
+
+const isValidToken = (t: string) => {
+  if (Expo.isExpoPushToken(t)) return t;
+  console.error(`Push token ${t} is not a valid Expo push token`);
+  return;
+};
+
+type NotificationWithHeaders = Notification & {
+  headers: Headers;
+};
+
+const composeGenericNotification = (n: NotificationWithHeaders) => ({
+  title: n.subject
+    .replace(`Re: ${n.headers["x-gitlab-project"]} | `, "")
+    .trimStart(),
+});
+
+const composePipelineNotification = (n: NotificationWithHeaders) => {
+  const status = n.headers["x-gitlab-pipeline-status"];
+  const STATUS_TITLE: Record<PipelineStatus, string> = {
+    success: "Pipeline Succeded!",
+    failed: "Pipeline Failed!",
+  };
+
+  return {
+    title: status ? STATUS_TITLE[status] : "Pipeline",
+    body: n.subject
+      .replace(`${n.headers["x-gitlab-project"]} | `, "")
+      .trimStart(),
+  };
+};
+
+const composeNotificationContent = (n: NotificationWithHeaders) => {
+  // We treat a notification as generic if it doesn't have a project associated
+  const isGeneric = !n.headers["x-gitlab-project"];
+  const isPipeline = !!n.headers["x-gitlab-pipeline-id"];
+
+  if (isGeneric) {
+    return composeGenericNotification(n);
+  }
+
+  if (isPipeline) {
+    return composePipelineNotification(n);
+  }
+
+  const title = n.subject
+    // Removing "Re: 'project-name'" used normally for emails
+    .replace(`Re: ${n.headers["x-gitlab-project"]} | `, "")
+    .trimStart();
+
+  const body = n.text?.split("\n")[0].trim();
+  return {
+    title,
+    body,
+  };
+};
 
 const sanitizeText = (text: string) => {
   // Removing "Reply to this email..."
@@ -60,8 +119,9 @@ router.post("/webhook", multer().none(), async (req: Request, res) => {
     return;
   }
 
+  let notification: Notification;
   try {
-    const notification = await prisma.notification.create({
+    notification = await prisma.notification.create({
       data: {
         ...data,
         headers,
@@ -70,14 +130,41 @@ router.post("/webhook", multer().none(), async (req: Request, res) => {
         contentHash,
         userId: user.id,
       },
-      select: NOTIFICATION_ESSENTIALS_FIELDS,
     });
-
-    res.status(201).json(notification);
   } catch (error) {
     console.error(error);
     res.status(500).end();
     return;
+  }
+
+  const pushTokens = user.expoPushTokens.filter(isValidToken);
+
+  if (pushTokens.length === 0) {
+    console.warn("User doesn't have any valid token");
+    return res.status(400).json({
+      message: "User doesn't have any valid token",
+    });
+  }
+
+  const notificationPayload: ExpoPushMessage = {
+    to: pushTokens,
+    sound: "default",
+    ...composeNotificationContent(notification as NotificationWithHeaders),
+    data: { nid: notification.id },
+  };
+
+  console.log("Notification payload:", notificationPayload);
+
+  const chunks = expo.chunkPushNotifications([notificationPayload]);
+
+  for (const chunk of chunks) {
+    try {
+      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+      console.info(ticketChunk);
+      // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+    } catch (error) {
+      console.error(error);
+    }
   }
 });
 
