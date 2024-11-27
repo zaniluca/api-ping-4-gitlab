73da59672087f343b80a37d39bcda847cd5a84df
Luca Zani <lucazani2002@gmail.com>
2024-11-27 22:03:53 +0100

    chore(sentry): release now uses railway provided env

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,14 +1,20 @@
 import * as Sentry from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
+// https://docs.railway.com/reference/variables#git-variables
+const release =
+  process.env.RAILWAY_GIT_COMMIT_SHA ?? process.env.SENTRY_RELEASE;
+
+console.log("Sentry release:", release);
+
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   enabled: process.env.NODE_ENV === "production",
   integrations: [nodeProfilingIntegration(), Sentry.prismaIntegration()],
   tracesSampleRate: 0.05,
   profilesSampleRate: 0.05,
+  release,
   // This env should be implicitly used by the SDK but let's make it explicit
-  release: process.env.SENTRY_RELEASE,
   environment: process.env.SENTRY_ENVIRONMENT,
   beforeSendTransaction(event) {
     // Remove the cursor from the transaction
