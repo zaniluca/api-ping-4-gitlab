4d7eaab343f31d27851fdd92e71b3646240ec2ce
Luca Zani <lucazani2002@gmail.com>
2025-01-10 14:07:59 +0100

    feat: added `apple-app-site-association` file for webcredentials (#55)

--- src/index.ts
+++ src/index.ts
@@ -55,6 +55,9 @@ app.get("/account-deletion-info", (_req, res) =>
   )
 );
 
+// Static files
+app.use(express.static("static"));
+
 // Error handling
 Sentry.setupExpressErrorHandler(app, {
   shouldHandleError: (error) => {
