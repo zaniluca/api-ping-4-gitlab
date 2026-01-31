5d282307e8567a04aa9c5835df17a0b6f3cd2a61
Luca Zani <lucazani2002@gmail.com>
2026-01-31 18:08:07 +0100

    feat: staging env

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,9 @@
 // Augment the Cloudflare namespace to add custom env vars
 declare namespace Cloudflare {
   interface Env {
+    SENTRY_RELEASE?: string;
     ANDROID_EMULATOR?: string;
     EXPO_ACCESS_TOKEN?: string;
+    ENVIRONMENT: "production" | "staging" | "development" | "test";
   }
 }
